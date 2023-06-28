8f2b3e3b0d75de868c90e063927b4367eddd51b0
jenbanim <jenbanim@gmail.com>
2023-06-28 04:06:19 +0000

    clean up history.js

--- www/history.js
+++ www/history.js
@@ -7,43 +7,46 @@
 // actually fucking insane? Am I in a nightmare right now? Am I in hell?
 
 
-document.getElementById("search-group-name").addEventListener("keypress", function(e) {
-    if (e.key === "Enter") {
+function main() {
+    document.getElementById("search-group-name").addEventListener("keypress", function(e) {
+        if (e.key === "Enter") {
+            search_ping_log();
+        }
+    });
+    reset_date();
+    const params = new URLSearchParams(window.location.search);
+    // stupid logic, don't care
+    if (params.size === 1) {
+        document.getElementById("search-group-name").value = params.get("group_name").toUpperCase();
         search_ping_log();
+    } else if (params.size === 2) {
+        document.getElementById("search-count").value = params.get("count");
+        document.getElementById("search-group-name").value = params.get("group_name").toUpperCase();
+        search_ping_log();
+    } else if (params.size > 1) {
+        document.getElementById("search-group-name").value = params.get("group_name").toUpperCase();
+        document.getElementById("search-count").value = params.get("count");
+        const epoch = new Date(params.get("epoch_sec") * 1000);
+        epoch.setMinutes(epoch.getMinutes() - epoch.getTimezoneOffset());
+        document.getElementById("search-date").value = epoch.toISOString().slice(0,19);
+        if (params.get("sort") === "ASC") {
+            document.getElementById("search-after").checked = true;
+        }
     }
-});
-reset_date();
-params = new URLSearchParams(window.location.search);
-// stupid logic, don't care
-if (params.size === 1) {
-    document.getElementById("search-group-name").value = params.get("group_name").toUpperCase();
     search_ping_log();
-} else if (params.size === 2) {
-    document.getElementById("search-count").value = params.get("count");
-    document.getElementById("search-group-name").value = params.get("group_name").toUpperCase();
-    search_ping_log();
-} else if (params.size > 1) {
-    document.getElementById("search-group-name").value = params.get("group_name").toUpperCase();
-    document.getElementById("search-count").value = params.get("count");
-    let epoch = new Date(params.get("epoch_sec") * 1000);
-    epoch.setMinutes(epoch.getMinutes() - epoch.getTimezoneOffset());
-    document.getElementById('search-date').value = epoch.toISOString().slice(0,19)
-    if (params.get("sort") === "ASC") {
-        document.getElementById("search-after").checked = true;
-    }
 }
-search_ping_log();
 
 
 function reset_date() {
-    let now = new Date();
+    const now = new Date();
     now.setMinutes(now.getMinutes() - now.getTimezoneOffset());
-    document.getElementById('search-date').value = now.toISOString().slice(0,19)
+    document.getElementById("search-date").value = now.toISOString().slice(0,19);
+    document.getElementById("search-before").checked = true;
 }
 
 
 function get_selected_sort() {
-    selected_sort = document.querySelector('input[name="search_type"]:checked').id;
+    const selected_sort = document.querySelector("input[name='search_type']:checked").id;
     if (selected_sort == "search-before") {
         return "DESC";
     }
@@ -52,23 +55,22 @@ function get_selected_sort() {
 
 
 function search_ping_log() {
-    let group_name = document.getElementById("search-group-name").value.toUpperCase();
-    let search_date = new Date(document.getElementById("search-date").value);
-    let epoch_sec = Math.floor(search_date.getTime() / 1000);
-    let sort;
-    sort = get_selected_sort();
-    let count = document.getElementById("search-count").value;
-    params = `?group_name=${group_name}&epoch_sec=${epoch_sec}&sort=${sort}&count=${count}`;
-    history.pushState({}, "", `${window.location.origin}${window.location.pathname}${params}`);
+    const group_name = document.getElementById("search-group-name").value.toUpperCase();
+    const search_date = new Date(document.getElementById("search-date").value);
+    const epoch_sec = Math.floor(search_date.getTime() / 1000);
+    const sort = get_selected_sort();
+    const count = document.getElementById("search-count").value;
+    const query = `?group_name=${group_name}&epoch_sec=${epoch_sec}&sort=${sort}&count=${count}`;
+    history.pushState({}, "", `${window.location.origin}${window.location.pathname}${query}`);
 
     fetch(
         `api/get_ping_log?group_name=${group_name}&sort=${sort}&epoch_sec=${epoch_sec}&count=${count}`
-    ).then((response) => {
+    ).then(function (response) {
         if (response.status != "200") {
-            alert("Group not found. Please try again.")
+            alert("Group not found. Please try again.");
         }
-        return response.json()
-    }).then((data) => {
+        return response.json();
+    }).then(function (data) {
         if (data.length === 0) {
             document.getElementById("search-results").innerHTML = "No results found - try a different search";
             return;
@@ -79,22 +81,22 @@ function search_ping_log() {
         results.setAttribute("search-count", count);
         const script = document.createElement("script");
         script.charset = "UTF-8";
-        for (var idx = 0; idx < data.length; idx++) {
-            let created_utc = data[idx][0];
-            let result_element = document.createElement("div");
+        for (let idx = 0; idx < data.length; idx++) {
+            const created_utc = data[idx][0];
+            const result_element = document.createElement("div");
             result_element.classList.add("search-result");
             result_element.setAttribute("created_utc", created_utc);
-            let date_element = document.createElement("div");
-            let date = new Date(data[idx][0]*1000 ).toLocaleString();
-            let date_text = document.createTextNode(date);
+            const date_element = document.createElement("div");
+            const date = new Date(data[idx][0]*1000 ).toLocaleString();
+            const date_text = document.createTextNode(date);
             date_element.appendChild(date_text);
-            let comment_element = document.createElement("blockquote");
+            const comment_element = document.createElement("blockquote");
             comment_element.setAttribute("class", "reddit-embed-bq");
             comment_element.setAttribute("data-embed-locale","en-EN");
             comment_element.setAttribute("data-embed-context","1");
             comment_element.setAttribute("data-embed-depth","2");
             comment_element.setAttribute("data-embed-height","340");
-            let link_element = document.createElement("a");
+            const link_element = document.createElement("a");
             link_element.href = `https://reddit.com${data[idx][2]}`;
             comment_element.appendChild(link_element);
             result_element.appendChild(date_element);
@@ -107,16 +109,16 @@ function search_ping_log() {
         // if it's really just this unweildy. I _want_ to just create the pagination object
         // once and then duplicate it, but there's no way to do that while preserving event
         // listeners... which is kinda important for buttons
-        pagination_top = document.createElement("span");
-        pagination_bottom = document.createElement("span");
-        previous_button_top = document.createElement("button");
-        previous_button_bottom = document.createElement("button");
+        const pagination_top = document.createElement("span");
+        const pagination_bottom = document.createElement("span");
+        const previous_button_top = document.createElement("button");
+        const previous_button_bottom = document.createElement("button");
         previous_button_top.appendChild(document.createTextNode("Older"));
         previous_button_bottom.appendChild(document.createTextNode("Older"));
         previous_button_top.addEventListener("click", load_previous);
         previous_button_bottom.addEventListener("click", load_previous);
-        next_button_top = document.createElement("button");
-        next_button_bottom = document.createElement("button");
+        const next_button_top = document.createElement("button");
+        const next_button_bottom = document.createElement("button");
         next_button_top.appendChild(document.createTextNode("Newer"));
         next_button_bottom.appendChild(document.createTextNode("Newer"));
         next_button_top.addEventListener("click", load_next);
@@ -133,22 +135,23 @@ function search_ping_log() {
 
 
 function load_previous() {
-    results_container = document.getElementById("search-results-list");
-    group_name = results_container.getAttribute("search-group-name");
-    epoch_sec = results_container.lastChild.getAttribute("created_utc");
-    count = results_container.getAttribute("search-count");
-    sort = get_selected_sort();
-    params = `?group_name=${group_name}&epoch_sec=${epoch_sec}&sort=DESC&count=${count}`
-    window.location.replace(`${window.location.origin}${window.location.pathname}${params}`)
+    const results_container = document.getElementById("search-results-list");
+    const group_name = results_container.getAttribute("search-group-name");
+    const epoch_sec = results_container.lastChild.getAttribute("created_utc");
+    const count = results_container.getAttribute("search-count");
+    const params = `?group_name=${group_name}&epoch_sec=${epoch_sec}&sort=DESC&count=${count}`;
+    window.location.replace(`${window.location.origin}${window.location.pathname}${params}`);
 }
 
 
 function load_next() {
-    results_container = document.getElementById("search-results-list");
-    group_name = results_container.getAttribute("search-group-name");
-    epoch_sec = results_container.lastChild.getAttribute("created_utc");
-    count = results_container.getAttribute("search-count");
-    sort = get_selected_sort();
-    params = `?group_name=${group_name}&epoch_sec=${epoch_sec}&sort=ASC&count=${count}`
-    window.location.replace(`${window.location.origin}${window.location.pathname}${params}`)
+    const results_container = document.getElementById("search-results-list");
+    const group_name = results_container.getAttribute("search-group-name");
+    const epoch_sec = results_container.lastChild.getAttribute("created_utc");
+    const count = results_container.getAttribute("search-count");
+    const params = `?group_name=${group_name}&epoch_sec=${epoch_sec}&sort=ASC&count=${count}`;
+    console.log(params);
+    window.location.replace(`${window.location.origin}${window.location.pathname}${params}`);
 }
+
+main()
